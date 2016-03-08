30f7114cd4ba0ed2ef78280def560dd5c9916808
LGaljo <luka.galjot@gmail.com>
2016-03-08 23:27:06 +0100

    Fixes

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -166,8 +166,6 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	static DEFINE_RATELIMIT_STATE(lmk_rs, DEFAULT_RATELIMIT_INTERVAL, 1);
 #endif
 
-	unsigned long nr_cma_free;
-	struct reclaim_state *reclaim_state = current->reclaim_state;
 #if defined(CONFIG_CMA_PAGE_COUNTING)
 	unsigned long nr_cma_inactive_file;
 	unsigned long nr_cma_active_file;
